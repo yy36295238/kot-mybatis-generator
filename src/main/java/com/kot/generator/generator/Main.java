b0c557fc2835a44bc020b55b6854835a420b2508
YangYu <36295238@qq.com>
2019-05-06 18:08:52 +0800

    init...

--- src/main/java/com/kot/generator/generator/Main.java
+++ src/main/java/com/kot/generator/generator/Main.java
@@ -11,9 +11,9 @@ import org.springframework.util.StopWatch;
 public class Main {
 
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://www.kakrot.com:3306/kakrot?serverTimezone=UTC&useSSL=false";
-    public static final String USERNAME = "kakrot";
-    public static final String PASSWORD = "Kakrot!1";
+    public static final String URL = "jdbc:mysql://localhost:3306/kakrot?serverTimezone=UTC&useSSL=false";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "123456";
 
     static final String AUTHOR = "yangyu";
 
